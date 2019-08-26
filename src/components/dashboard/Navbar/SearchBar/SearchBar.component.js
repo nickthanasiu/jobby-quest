86159ac0afa2865250ac36046a0004044978ddb8
Nicholas Thanasiu <nick.thanasiu@gmail.com>
2019-08-26 18:53:32 -0400

    Style SearchBar component

--- src/components/dashboard/Navbar/SearchBar/SearchBar.component.js
+++ src/components/dashboard/Navbar/SearchBar/SearchBar.component.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { MdSearch } from 'react-icons/md';
 
 // Styles
 import './SearchBar.styles.scss';
 
 const SearchBar = () => {
     return (
-        <div className="search-bar-wrapper">
-            <input className="search-bar" />
-        </div>
+        <form className="search-bar-wrapper">
+            <input 
+                type="text"
+                className="search-bar" 
+                placeholder="Search your jobs"    
+            />
+            <button type="submit" className="search-btn">
+                <MdSearch color="#fff" />
+            </button>
+        </form>
     );
 };
 
